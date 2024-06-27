05567a24507d931d9d9380e789d39a224c600294
Abin56 <abinjohn8089@gmail.com>
2024-06-27 15:43:30 +0530

    first commit --@edwin

--- lib/controller/notification_controller/notification_Controller.dart
+++ lib/controller/notification_controller/notification_Controller.dart
@@ -239,41 +239,71 @@ class NotificationController extends GetxController {
       }
     }
   }
-
-  Future<void> sendPushMessage(String token, String body, String title) async {
-    try {
-      final reponse = await http.post(
-        Uri.parse('https://fcm.googleapis.com/fcm/send'),
-        headers: <String, String>{
-          'Content-Type': 'application/json',
-          'Authorization':
-              'key=AAAAT5j1j9A:APA91bEDY97KTVTB5CH_4YTnLZEol4Z5fxF0fmO654V7YJO6dL9TV_PyIfv64-pVDx477rONsIl8d63VjxT793_Tj4zuGg32JTy_wUNQ4OhGNbr0KOS2i4z7JaG-ZtENTBpYnEGh-ZLg'
-        },
-        body: jsonEncode(
-          <String, dynamic>{
-            'priority': 'high',
-            'data': <String, dynamic>{
-              'click_action': 'FLUTTER_NOTIFICATION_CLICK',
-              'status': 'done',
-              'body': body,
-              'title': title,
-            },
-            "notification": <String, dynamic>{
-              'title': title,
-              'body': body,
-              'android_channel_id': 'high_importance_channel'
-            },
-            'to': token,
-          },
-        ),
-      );
-      log(reponse.body.toString());
-    } catch (e) {
-      if (kDebugMode) {
-        log("error push Notification");
-      }
-    }
+  RxString pushNotficationKey = ''.obs;
+  getPushNotification() async {
+    FirebaseFirestore.instance
+        .collection('PushNotification')
+        .doc('key')
+        .get()
+        .then((value) async {
+      pushNotficationKey.value = value.data()?['key'];
+    });
   }
+  Future<void> sendPushMessage(String token, String body, String title) async {
+   await  getPushNotification();
+    final serverKey = pushNotficationKey.value;
+    final Uri url = Uri.parse(
+        'https://fcm.googleapis.com/v1/projects/kalyanschools-b1b08/messages:send');
+
+    final Map<String, dynamic> message = {
+      'message': {
+        'token': token,
+        'notification': {
+          'title': title,
+          'body': body,
+        },
+        'android': {
+          'notification': {
+            'title': title,
+            'body': body,
+            'click_action': 'TOP_STORY_ACTIVITY'
+          },
+          'data': {'story_id': 'story_12345'}
+        },
+        'apns': {
+          'payload': {
+            'aps': {'category': 'NEW_MESSAGE_CATEGORY'}
+          },
+        },
+        'data': {
+          'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+          'status': 'done',
+          'body': body,
+          'title': title,
+        },
+      },
+    };
+
+    try {
+      final response = await http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $serverKey',
+        },
+        body: jsonEncode(message),
+      );
+
+      if (response.statusCode == 200) {
+        print('Notification sent successfully!');
+      } else {
+        print('Failed to send notification: ${response.statusCode}');
+        print('Response body: ${response.body}');
+      }
+    } catch (e) {
+      print('Exception caught sending notification: $e');
+    }
+  } 
 
   Future<void> userparentNotification({
     required String parentID,
