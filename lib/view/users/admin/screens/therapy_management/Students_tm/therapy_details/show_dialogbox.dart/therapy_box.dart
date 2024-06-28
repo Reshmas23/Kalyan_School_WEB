6eb3787a2e817afcbfa3a8d42d612ebd07ec3681
Reshmas23 <reshmasuresh8089@gmail.com>
2024-06-28 11:18:31 +0530

    june 28 repush code --@ reshma

---
+++ lib/view/users/admin/screens/therapy_management/Students_tm/therapy_details/show_dialogbox.dart/therapy_box.dart
@@ -0,0 +1,7 @@
+import 'package:vidyaveechi_website/view/web_DashBoard/pages/video_management/presentation/pages/widgets/video_widgets.dart';
+import 'package:vidyaveechi_website/view/widgets/custom_showDilog/show_dialoge_box.dart';
+
+// detailsOfStudentsInTherapy(BuildContext context){
+//   showDialogeBox(context: context,
+//   children: Column());
+// }
